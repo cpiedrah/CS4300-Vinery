f34ec76cae6954c3b376542a0538c12d57fe68ec
Carter Larsen <cjl259@cornell.edu>
2025-04-20 20:25:57 -0400

    Add Location and Point Rating Consideration
    Modified dataset to have location. The points are now considered when measuring similarity

--- backend/app.py
+++ backend/app.py
@@ -18,13 +18,14 @@ os.environ['ROOT_PATH'] = os.path.abspath(os.path.join("..",os.curdir))
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
 # Specify the path to the JSON file relative to the current script
-json_file_path = os.path.join(current_directory, 'cellartracker_wine_reviews_with_rating_40pages.json')
+# json_file_path = os.path.join(current_directory, 'cellartracker_wine_reviews_with_rating_40pages.json')
+json_file_path = os.path.join(current_directory, 'cellartracker_wine_reviews.json')
 
 # Assuming your JSON data is stored in a file named 'init.json'
 with open(json_file_path, 'r') as file:
     data = json.load(file)
     wines_df = pd.DataFrame(data)
-    documents = [(x['Wine Name'], x['Variety'], x['Review'], x['Rating'])
+    documents = [(x['Wine Name'], x['Variety'], x['Review'], x['Rating'], x['Location'])
                  for x in data if x['Review'] is not None and len(x['Review']) > 20]
     vectorizer = TfidfVectorizer(stop_words = 'english', max_df = .7, min_df = 1)
     td_matrix = vectorizer.fit_transform([x[2] for x in documents if x[2] is not None])
@@ -46,6 +47,21 @@ with open(json_file_path, 'r') as file:
     # Get the unique varieties from the DataFrame without None values
     unique_varieties = wines_df['Variety'].dropna().unique()
 
+
+    ratings = np.array([x[3].split(' ')[-2] for x in documents])
+
+    # if the rating is not a number, set it to 0
+    ratings = [float(r) if not ':' in r else 0 for r in ratings]
+    ratings = np.array(ratings)
+    ratings = ratings / 100
+
+    # load location map from location_dict_deduped.json
+    location_map = {}
+    with open(os.path.join(current_directory, 'location_dict_deduped.json'), 'r') as file:
+        location_map = json.load(file)
+    
+
+
 app = Flask(__name__)
 CORS(app)
 
@@ -61,8 +77,9 @@ def closest_wines(wine_index_in, wine_repr_in, k = 5):
     asort = np.argsort(-sims)[:k+1]
     return [(documents[i][0], documents[i][1], documents[i][2], documents[i][3], sims[i]) for i in asort[1:]]
 
-def closest_wines_to_query(query_vec_in, k = 5, variety_filter=None, year_filter=None, color_filter=None):
+def closest_wines_to_query(query_vec_in, k = 5, variety_filter=None, year_filter=None, color_filter=None, location_filter=None):
     sims = docs_compressed_normed.dot(query_vec_in)
+    sims = (sims + ratings) / 2
     asort = np.argsort(-sims)
     
     ROSE_KEYWORDS = ['rosé', 'rose']
@@ -90,9 +107,6 @@ def closest_wines_to_query(query_vec_in, k = 5, variety_filter=None, year_filter
         if any(keyword in v or keyword in t or keyword in d for keyword in RED_KEYWORDS):
             return 'red'
         return 'unknown'
-    # if both variety and year filter do not exist
-    # if variety_filter is None and year_filter is None:
-        # return [(documents[i][0], documents[i][1], documents[i][2], documents[i][3], sims[i]) for i in asort[1:]]
     
     added = 0
     res = []
@@ -101,7 +115,7 @@ def closest_wines_to_query(query_vec_in, k = 5, variety_filter=None, year_filter
         # Full metadata row
         wine_data = data[i]
 
-        title, variety, review, rating = documents[i][:4]
+        title, variety, review, rating, location = documents[i][:5]
         designation = wine_data.get("designation", [""])[0] if isinstance(wine_data.get("designation"), list) else ""
         # FILTERS
         if variety_filter and (not variety or variety_filter.lower() not in variety.lower()):
@@ -110,8 +124,9 @@ def closest_wines_to_query(query_vec_in, k = 5, variety_filter=None, year_filter
             continue
         if color_filter and classify_color(variety, title, designation) != color_filter:
             continue
-
-        res.append((title, variety, review, rating, sims[i]))
+        if location_filter and (not location or (location.lower() not in location_filter.lower() and all(location.lower() not in loc.lower() for loc in location_map.get(location_filter, [])))):
+            continue
+        res.append((title, variety, review, rating, location, sims[i]))
         added += 1
         if added == k:
             break
@@ -122,8 +137,8 @@ def closest_wines_to_query(query_vec_in, k = 5, variety_filter=None, year_filter
 def json_search(query):
     variety_filter = None
     year_filter = None
-
     color_filter = None
+    location_filter = None
 
     if query:
         # Search is case-insensitive
@@ -166,43 +181,46 @@ def json_search(query):
             if variety_name in query_lower:
                 color_filter = color
                 break
-            # If the query contains any of the varieties in unique varieties
+        # If the query contains any of the varieties in unique varieties
         for variety in unique_varieties:
-            if variety.lower() in query_lower:
+            # Need to add spaces to avoid matching 'licorice' with 'rice' as a varietal
+            if f" {variety.lower()} " in f" {query_lower} ":
                 variety_filter = variety
                 query_lower = query_lower.replace(variety.lower(), '')
                 break  # Stop at first match
-            # If the query contains a year (4 digits)
+        # If the query contains a year (4 digits)
         for word in query_lower.split():
             if len(word) == 4 and word.isdigit():
                 query_lower = query_lower.replace(word, '')
                 year_filter = word
-            # Get tf-idf representation of the query
-        query_tfidf = vectorizer.transform([query_lower]).toarray()
-            # Normalize the query vector
-        query_vec = normalize(np.dot(query_tfidf, words_compressed)).squeeze()
-            # Find the closest wines to the query vector
-        matches = closest_wines_to_query(query_vec, k = 20, variety_filter=variety_filter, year_filter=year_filter)
-        # Convert matches to DataFrame for easier manipulation
+        for location in location_map.keys():
+            if location.lower() in query_lower:
+                query_lower = query_lower.replace(location.lower(), '')
+                location_filter = location
 
-        # matches = pd.DataFrame(matches, columns=['Wine Name', 'Variety', 'Review', 'Rating', 'Similarity'])
+        # Get tf-idf representation of the query
+        query_tfidf = vectorizer.transform([query_lower]).toarray()
+        # Normalize the query vector
+        query_vec = normalize(np.dot(query_tfidf, words_compressed)).squeeze()
+        # Find the closest wines to the query vector
         matches = closest_wines_to_query(
         query_vec,
         k=20,
         variety_filter=variety_filter,
         year_filter=year_filter,
         color_filter=color_filter,
+        location_filter=location_filter
         )
     else:
         matches = wines_df.copy()
         matches['Similarity'] = None
 
-        columns_to_use = ['Wine Name', 'Review', 'Rating', 'Variety']
+        columns_to_use = ['Wine Name', 'Review', 'Rating', 'Location', 'Variety']
         columns_present = [col for col in columns_to_use if col in matches.columns]
         matches = matches[columns_present]
     
-    matches = pd.DataFrame(matches, columns=['Wine Name', 'Variety', 'Review', 'Rating', 'Similarity'])
-    matches_filtered = matches[['Wine Name', 'Review', 'Rating', 'Variety', 'Similarity']]
+    matches = pd.DataFrame(matches, columns=['Wine Name', 'Variety', 'Review', 'Rating', 'Location', 'Similarity'])
+    matches_filtered = matches[['Wine Name', 'Review', 'Rating', 'Variety', 'Location', 'Similarity']]
 
     
     return matches_filtered.to_json(orient='records')
